48e3b2529d7a8d27d6d664abe71d1dcea257edcf
molnark <molnark@gmail.com>
2015-04-26 16:58:19 -0400

    My Edits
    I'm assuming i have no control over what's in red? Anyway, here's
    Assignment two.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
+
+#THis function accepts a matrix, and returns a new object 
+#that allows caching
 
 makeCacheMatrix <- function(x = matrix()) {
+m<- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get<-function()x
+  setInv <- function(inverse) m <<- inverse
+  getInv <- function() m
+  list(set = set, get = get,
+       setInv = setInv,
+       getInv = getInv)
+
+
 
 }
 
-
-## Write a short comment describing this function
-
+#This is the workhorse function. It grabs the matrix,and calculates the inverse of that matrix
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+       
+  m <- x$getInv()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setInv(m)
+# Returns the inverse of matrix
+  m
 }
